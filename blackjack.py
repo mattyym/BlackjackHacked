66ecf705960f9cf553ac15dbf7f2a416338a2d9b
Dalton McLaughlin <mclau237@msu.edu>
2026-01-31 16:05:56 -0500

    Main Game Loop with Betting System

--- blackjack.py
+++ blackjack.py
@@ -2,11 +2,13 @@ import random
 
 # Create the card deck
 cards = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
-deck = list(cards.keys()) * 4
+player_hand = []
+dealer_hand = []
+player_points = 1000
 
 # Function to pick a random card
 def deal_card():
-    return random.choice(deck)
+    return deck.pop(0)
 
 # Function to return the full value of a hand
 def hand_value(hand):
@@ -23,6 +25,17 @@ def show_hand(hand, owner):
 
 # Main blackjack gameplay function
 def blackjack():
+    global player_points
+    # Initiate bet amount
+    while True:
+        print(f"You have {player_points} points.")
+        bet = input("Enter your bet amount (Win -> Bet x 2): ")
+        if bet.isdigit() and 0 < int(bet) <= player_points:
+            bet = int(bet)
+            break
+        else:
+            print("Invalid bet amount. Please try again.")
+
     # Initial Deal
     player_hand = [deal_card(), deal_card()]
     dealer_hand = [deal_card(), deal_card()]
@@ -43,7 +56,9 @@ def blackjack():
 
     # If player is over 21 they bust
     if hand_value(player_hand) > 21:
-        print("You busted, the dealer smoked yo buns!!")
+        player_points -= bet
+        print("You busted, the dealer smoked yo buns!!\n")
+        print(f"You now have {player_points} points.\n")
         return
 
     # Show the dealers hand and have it hit or stand for its turn
@@ -63,12 +78,27 @@ def blackjack():
 
     # Establish winner and display that
     if dealer_total > 21 or player_total > dealer_total:
+        player_points += bet
         print("\nYou won!\n")
+        print(f"You now have {player_points} points.\n")
     elif player_total < dealer_total:
+        player_points -= bet
         print("\nYou lost!\n")
+        print(f"You now have {player_points} points.\n")
     else:
         print("\nDraw!\n")
+        print(f"You still have {player_points} points.\n")
 
 # Main stuff to run the program
 if __name__ == "__main__":
-    blackjack()
+    while True:
+        deck = list(cards.keys()) * 4
+        random.shuffle(deck)
+        if player_points <= 0:
+            print(f"\nYou are out of points!\n")
+            print("You suck! Get some bread. Game over.")
+            break
+        blackjack()
+        play_again = input("Play again? (y/n): ").lower()
+        if play_again != "y":
+            break
