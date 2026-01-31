4678407da021c8e3733176e4ce84742ffe1d874e
Matt <matt.mifsud@me.com>
2026-01-31 16:42:31 -0500

    refactor: changes to power up functions

--- power_ups/powerUp.py
+++ power_ups/powerUp.py
@@ -1,27 +1,28 @@
 def Peek(deck: list) -> string:
-    return "The next card in the deck is: ", deck.pop()
+    return "The next card in the deck is: ", deck[-1]
 
 def xRay(dealerHand: list) -> string:
-    return "The dealers next card is: ", deck.pop()
+    return "The dealers next card is: ", deck[-1]
 
 def Insurance(betAmt: int):
+    # lock losses to 50%
     return "Cut your losses in half this hand!"    
 
-def Duplicate(deck: list, playerHand: list): 
-    result = deck.pop()
-    playerHand.append(result)
-    playerHand.append(result)
+def Duplicate(deck: list): 
+    return deck.pop()
 
 def DrawTwo(deck: list):
     resultOne = deck.pop()
     resultTwo = deck.pop()
-
-    return resultOne, resultTwo
+    response = input("The first card is: ", resultOne, " do you want to keep this card? No to draw again (Y/N)")
+    if response == "N":
+        print("The card: ", resultTwo, "will be added to your hand")
+        return resultTwo
+    else:
+        print("The card: ", resultOne, "Will be added to your hand")
+        return resultOne
 
 def Mulligan(deck: list):
-    resultOne = deck.pop()
-    resultTwo = deck.pop()
-
-    return resultOne, resultTwo
+    return deck.pop()
 
 
